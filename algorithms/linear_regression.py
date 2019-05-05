5467b4e06926ee3d290d7809d5d9f7e83d374b19
Vilma Bezerra Alves <vilmabezerraalves@gmail.com>
2019-05-05 18:42:41 -0300

    Add two out of three questions related to nonlinear regression

--- algorithms/linear_regression.py
+++ algorithms/linear_regression.py
@@ -9,7 +9,6 @@ def run(points, points_classification):
 
 	return np.dot(points_pseudo_inverse, points_classification).T, points_matrix
 
-
 def classify_points_given_weights(points_matrix, weights):
 	return np.sign(np.dot(points_matrix, weights))
 
@@ -17,3 +16,19 @@ def classify_points_given_weights(points_matrix, weights):
 def get_points_adapted_matrix(points):
 	return np.matrix([np.insert(point, 0, 1, axis=0) 
 		for point in points])
+
+
+def get_transformed_points_matrix(points):
+	points = np.insert(points, 0, 1, axis=1) 
+	points = np.insert(points, 3, values=points[:,1]*points[:,2], axis=1) 
+	points = np.insert(points, 4, values=points[:,1]**2, axis=1)
+	points = np.insert(points, 5, values=points[:,2]**2, axis=1)
+	return np.matrix(points)
+
+def run_with_transformation(points, points_classification):
+	points_matrix = get_transformed_points_matrix(points)
+
+	points_pseudo_inverse = np.dot(np.linalg.inv(np.dot(points_matrix.T, points_matrix)), 
+		points_matrix.T)
+
+	return np.dot(points_pseudo_inverse, points_classification).T, points_matrix
--- toys/second_one.py
+++ toys/second_one.py
@@ -131,8 +131,77 @@ def linear_regression_part2(n_points, run_times=1000, y_min=-1, y_max=1):
 
 	print(f'Average iterations: {np.mean(iterations_list)}')
 
+def add_noise_to_points_classification(points_classification, n_points, 
+	percentage=0.1):
+	points_classification_copy = points_classification[:]
+	for index in range(0, int(n_points*percentage)):
+		points_classification_copy[index] *= -1
+	return points_classification_copy
+
+def calculate_avg_E_in_adding_noise_to_data(n_points, run_times, y_min, y_max):
+	in_sample_errors = list()
+	for i in range(run_times):
+		target_function = get_target_function()
+		points = np.random.uniform(y_min, y_max, size=(n_points, 2))
+		up_line_classification = np.random.choice([y_max, y_min])
+
+		points_classification = get_uniform_random_points_classification(
+			points, target_function, up_line_classification)
+
+		# noisy_points_classification = add_noise_to_points_classification(
+			# points_classification, n_points)
+
+		weights, points_matrix = linear_regression.run(points, 
+			points_classification)
+
+		linear_regression_classification = \
+			linear_regression.classify_points_given_weights(points_matrix, 
+				weights)
+
+		in_sample_error = sum([1 for i, x in enumerate(points_classification) 
+			if x != linear_regression_classification[i]]) / n_points
+
+		output_str = f'iteration: {i}, E_in: {in_sample_error}'
+		print(output_str, end='\r'*len(output_str))
+
+		in_sample_errors.append(in_sample_error)
+
+	return in_sample_errors
+
+def calculate_average_weights_when_transforming_data(n_points, run_times, 
+	y_min, y_max):
+	weights_list = list()
+
+	for i in range(run_times):
+		target_function = get_target_function()
+		points = np.random.uniform(y_min, y_max, size=(n_points, 2))
+		up_line_classification = np.random.choice([y_max, y_min])
+
+		points_classification = get_uniform_random_points_classification(
+			points, target_function, up_line_classification)
+
+		weights, _ = linear_regression.run_with_transformation(points, 
+			points_classification)
+		weights_list.append(np.ravel(weights))
+
+	return [np.mean([weights[index] for weights in weights_list]) 
+		for index in range(len(weights))]
+
+def nonlinear_regression_part(n_points=1000, run_times=1000, 
+	y_min=-1, y_max=1):
+
+	in_sample_errors = \
+		calculate_avg_E_in_adding_noise_to_data(n_points, run_times, y_min, y_max)
+
+	print('Average E_in when data is noisy: {}'.format(np.mean(in_sample_errors)))
+
+	average_weights = calculate_average_weights_when_transforming_data(n_points, 
+		run_times, y_min, y_max)
+	print(f'Average weights when data is transformed: {average_weights}')
+
 
 if __name__ == '__main__':
 	hoeffding_inequality_part()
 	linear_regression_part1(n_points=100)
 	linear_regression_part2(n_points=10)
+	nonlinear_regression_part(n_points=1000)
