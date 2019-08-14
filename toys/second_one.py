fb638c2db929aade01cc811c978b1294ccf652a2
Vilma Bezerra Alves <vilmabezerraalves@gmail.com>
2019-08-13 21:59:49 -0300

    Add nonlinear regresion Eout calculation

--- toys/second_one.py
+++ toys/second_one.py
@@ -15,8 +15,10 @@ def get_coin_min_index(heads_result, tosses):
 			min_heads = x
 	return coin_min_index
 
+
 def calculate_coin_fraction(coin_index, heads_result, tosses):
 	return heads_result[coin_index] / tosses
+
 
 def run_experiment(coins_quantity, tosses):
 	head = 1
@@ -38,6 +40,7 @@ def run_experiment(coins_quantity, tosses):
 
 	return coin1_fraction, coin_rand_fraction, coin_min_fraction
 
+
 def hoeffding_inequality_part(run_times=100000, coins_quantity=1000, tosses=10):
 	average_fractions = list()
 	for i in range(run_times):
@@ -50,6 +53,7 @@ def hoeffding_inequality_part(run_times=100000, coins_quantity=1000, tosses=10):
 	print('Average value of Cmin fraction: {}'.format(np.mean([i[2] for i in average_fractions])))
 	print('Average value of C1 fraction: {}'.format(np.mean([i[0] for i in average_fractions])))
 	print('Average value of Crand fraction: {}'.format(np.mean([i[1] for i in average_fractions])))
+
 
 def get_out_of_sample_errors(points, target_functions_list, up_line_classification_list, weights_list):
 	out_of_sample_errors = list()
@@ -70,6 +74,7 @@ def get_out_of_sample_errors(points, target_functions_list, up_line_classificati
 
 		out_of_sample_errors.append(out_of_sample_error)
 	return out_of_sample_errors
+
 
 def linear_regression_part1(n_points, run_times=1000, y_min=-1, y_max=1):
 	in_sample_errors = list()
@@ -132,12 +137,14 @@ def linear_regression_part2(n_points, run_times=1000, y_min=-1, y_max=1):
 
 	print(f'Average iterations: {np.mean(iterations_list)}')
 
+
 def add_noise_to_points_classification(points_classification, n_points, 
 	percentage=0.1):
 	points_classification_copy = points_classification[:]
 	for index in range(0, int(n_points*percentage)):
 		points_classification_copy[index] *= -1
 	return points_classification_copy
+
 
 def calculate_avg_E_in_adding_noise_to_data(n_points, run_times, y_min, y_max):
 	in_sample_errors = list()
@@ -148,17 +155,17 @@ def calculate_avg_E_in_adding_noise_to_data(n_points, run_times, y_min, y_max):
 		get_uniform_random_points_classification_with_predefined_target_function(
 			points)
 
-		# noisy_points_classification = add_noise_to_points_classification(
-			# points_classification, n_points)
+		noisy_points_classification = add_noise_to_points_classification(
+			points_classification, n_points)
 
 		weights, points_matrix = linear_regression.run(points, 
-			points_classification)
+			noisy_points_classification)
 
 		linear_regression_classification = \
 			linear_regression.classify_points_given_weights(points_matrix, 
 				weights)
 
-		in_sample_error = sum([1 for i, x in enumerate(points_classification) 
+		in_sample_error = sum([1 for i, x in enumerate(noisy_points_classification) 
 			if x != linear_regression_classification[i]]) / n_points
 
 		output_str = f'iteration: {i}, E_in: {in_sample_error}'
@@ -167,6 +174,36 @@ def calculate_avg_E_in_adding_noise_to_data(n_points, run_times, y_min, y_max):
 		in_sample_errors.append(in_sample_error)
 
 	return in_sample_errors
+
+
+def calculate_avg_E_out_adding_noise_to_data(n_points, run_times, y_min, y_max, weights):
+	out_sample_errors = list()
+	for i in range(run_times):
+		points = np.random.uniform(y_min, y_max, size=(n_points, 2))
+
+		points_classification = \
+		get_uniform_random_points_classification_with_predefined_target_function(
+			points)
+
+		noisy_points_classification = add_noise_to_points_classification(
+			points_classification, n_points)
+
+		points_matrix = linear_regression.get_transformed_points_matrix(points)
+
+		linear_regression_classification = \
+			linear_regression.classify_points_given_weights(points_matrix, 
+				weights).T
+
+		out_sample_error = sum([1 for i, x in enumerate(noisy_points_classification) 
+			if x != linear_regression_classification[i]]) / n_points
+
+		output_str = f'iteration: {i}, E_out: {out_sample_error}'
+		print(output_str, end='\r'*len(output_str))
+
+		out_sample_errors.append(out_sample_error)
+
+	return out_sample_errors
+
 
 def calculate_average_weights_when_transforming_data(n_points, run_times, 
 	y_min, y_max):
@@ -186,6 +223,7 @@ def calculate_average_weights_when_transforming_data(n_points, run_times,
 	return [np.mean([weights[index] for weights in weights_list]) 
 		for index in range(len(weights))]
 
+
 def nonlinear_regression_part(n_points=1000, run_times=1000, 
 	y_min=-1, y_max=1):
 
@@ -198,8 +236,10 @@ def nonlinear_regression_part(n_points=1000, run_times=1000,
 		run_times, y_min, y_max)
 	print(f'Average weights when data is transformed: {average_weights}')
 
+	weights = np.array(average_weights)
+
 	out_of_sample_errors = \
-		calculate_avg_E_in_adding_noise_to_data(n_points, run_times, y_min, y_max)
+		calculate_avg_E_out_adding_noise_to_data(n_points, run_times, y_min, y_max, weights)
 	print('Average E_out when data is noisy: {}'.format(np.mean(out_of_sample_errors)))
 
 
